1221aefc5c473596619bbb6ee5dacf4f64f11b76
Treterten <mitch3moore@gmail.com>
2021-01-28 22:26:03 -0500

    Implemented a get method for the array

--- Array.py
+++ Array.py
@@ -1,12 +1,17 @@
 class Array():
-  def __init__(self, length, dataType):
+  def __init__(self, length, dataType, size):
     self.storage = {}
     self.length = length
+    self.size = size
     if (dataType != int or dataType != str or dataType != bool or dataType != object or dataType != float):
       raise TypeError('Invalid DataType')
     self.dataType = dataType
-  def add(self, index, item):
+  def setAt(self, index, item):
+    if (self.size == self.length):
+      raise ValueError('Array is at max capacity.')
     if (type(item) != self.dataType):
       raise TypeError('Invalid Type for Array<' + str(self.dataType) + '>')
     self.storage[index] = item
+  def get(self, index):
+    return self.storage[index]
 
