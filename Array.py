330fd98e129ed1d249bf3e5c2e1fc9ca5043a139
Treterten <mitch3moore@gmail.com>
2021-01-29 21:23:55 -0500

    Fixed issue with logic type checking

--- Array.py
+++ Array.py
@@ -3,21 +3,21 @@ class Array():
     self.storage = {}
     self.length = 0
     self.size = size
-    if (dataType != int or dataType != str or dataType != bool or dataType != object or dataType != float):
+    if (dataType != int and dataType != str and dataType != bool and dataType != object and dataType != float):
       raise TypeError('Invalid DataType')
     self.dataType = dataType
   def setAt(self, index, item):
     if (self.size == self.length):
       raise ValueError('Array is at max capacity.')
     if (type(item) != self.dataType):
-      raise TypeError('Invalid Type for Array<' + str(self.dataType) + '>')
+      raise TypeError('Invalid Type for Array')
     self.storage[index] = item
   def get(self, index):
     return self.storage[index]
 
 
 def testArray():
-  myArr = Array(str, 4)
-  myArr.setAt(0, 2)
+  myArr = Array(str, '4')
+  myArr.setAt(0, '2')
 
 testArray()
