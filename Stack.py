91780f75b851b5d74fcee41ce0df0b32ad522c01
Treterten <mitch3moore@gmail.com>
2021-02-02 22:04:37 -0500

    Added tests and implemented str function

--- Stack.py
+++ Stack.py
@@ -5,4 +5,17 @@ class Stack():
     self.storage.append(value)
   def pop(self):
     return self.storage.pop()
+  def __str__(self):
+    string = ''
+    for element in self.storage:
+      string += element + ' '
+    return string
 
+if __name__ == '__main__':
+  myStack = Stack()
+  myStack.add('hi')
+  myStack.add('oh no')
+  myStack.add('hehe')
+  print(myStack)
+  myStack.pop()
+  print(myStack)
