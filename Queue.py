3f7cf8d5af7880c21fea1d1421b4a2ec2d36a51d
Treterten <mitch3moore@gmail.com>
2021-02-02 22:26:31 -0500

    Added Queue tests

--- Queue.py
+++ Queue.py
@@ -3,6 +3,8 @@ class Queue():
     self.storage = []
   def enqueue(self, value):
     self.storage.append(value)
+  def __str__(self):
+    return ' '.join([str(i) for i in self.storage])
   def isEmpty(self):
     if (len(self.storage) == 0):
       return True
@@ -15,3 +17,17 @@ class Queue():
       return 'Empty'
   def getSize(self):
     return len(self.storage)
+
+if __name__ == '__main__':
+  myQueue = Queue()
+  myQueue.enqueue(3213)
+  myQueue.enqueue('Hi')
+  myQueue.enqueue('Over 90009')
+  print(myQueue)
+  myQueue.dequeue()
+  print(myQueue)
+  print(myQueue.isEmpty())
+  print(myQueue.getSize())
+  myQueue.dequeue()
+  myQueue.dequeue()
+  print(myQueue.isEmpty())
