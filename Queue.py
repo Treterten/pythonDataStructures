3ec570cae52f08084f8e7e49251ae9af3b348824
Treterten <mitch3moore@gmail.com>
2021-02-02 22:23:21 -0500

    Implemented methods for Queue

--- Queue.py
+++ Queue.py
@@ -1,3 +1,17 @@
 class Queue():
   def __init__(self):
     self.storage = []
+  def enqueue(self, value):
+    self.storage.append(value)
+  def isEmpty(self):
+    if (len(self.storage) == 0):
+      return True
+    else:
+      return False
+  def dequeue(self):
+    if (not self.isEmpty()):
+      return self.storage.pop(0)
+    else:
+      return 'Empty'
+  def getSize(self):
+    return len(self.storage)
